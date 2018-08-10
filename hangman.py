345481a1d0a204be6a5c5fbc48a4e3210034befb
ymnemutlu@gmail.com <l1155142>
2018-08-09 20:18:10 -0700

    first checkin for handman

---
+++ hangman.py
@@ -0,0 +1,34 @@
+secret_word=''
+
+def GetSecretWord():
+    global  secret_word
+    secret_word='maccoroni'
+
+def ShowHangMan(failed):
+    print(' +-------+')
+    if failed==0:
+        print('         |' )
+    if failed>0:
+        print('  O      |' )
+    if failed==2:
+        print(' / \\     |')
+    if failed>2:
+        print('/ | \\    |')
+    if failed>3:
+        print(' / \\     |')
+
+
+
+
+def main():
+    GetSecretWord()
+    ShowHangMan(4)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
