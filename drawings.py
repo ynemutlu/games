5db882a98908caef18784695881a57f7a60aa9a8
ymnemutlu@gmail.com <l1155142>
2018-12-01 00:43:16 -0800

    pushing in my beatiful cat

---
+++ drawings.py
@@ -0,0 +1,25 @@
+def DrawCat():
+    print("          ____________________________")
+    print('       __/xxx     xx       xxx     xx \\')
+    print('      /xxxxxxx     xx       xx     xx  \\_')
+    print('     /      xx                         xx\\  ')
+    print('    /           /\\                        \\')
+    print('    |   /\\     /  \\___                     \\_')
+    print('    |  /  \\___/       \\_            Z        \\')
+    print('    | /                  \\        z           \\')
+    print('    /              ___/  |   z                |')
+    print('  __|    \\___            |                 xxx| ')
+    print(' /  |           ___  ___-----              _ x|')
+    print('/   |          |   |   _____          _____/  |')
+    print('|    \\  ------  \\_/      /___________/      xx/')
+    print('\\      \\_  ----       __/                    /')
+    print(' \\       \\___________/                      /')
+    print('  \\_________________________________________/')
+def main():
+    DrawCat()
+
+
+
+
+if __name__ == "__main__":
+    main()
